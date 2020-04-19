12ae3607d851017772a73e2c6910361c2693b7a8
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-19 17:29:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,4 @@
 # Add code here!
 def prime?(number)
-  if number < 2
-    false
-  elsif n == 3 || n == 2
-    true
-  else
-  if 2..number.any?{|i| number % i == 0}
-    false
-  else
-    true
-  end
-end
 end
 # A prime number is divisible by itself and 1.
