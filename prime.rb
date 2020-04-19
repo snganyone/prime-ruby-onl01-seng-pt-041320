80f3bb1d584f6ab6a6f83472a36bcdcb30375ad5
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-19 16:04:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,7 @@
 # Add code here!
 def prime?(number)
   (-2..number).each do |num|
-    if number % num == 0 || number < 2
-      return false
-    else
-      return true
-    end
+    return false if number % num == 0
   end
+  true
 end
