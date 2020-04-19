ef9045d9e953b288b19d438ade8280c9444fe5f1
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-19 16:03:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+# Add code here!
+def prime?(number)
+  (-2..number).each do |num|
+    if number % num == 0 || number < 2
+      return false
+    else
+      return true
+    end
+  end
+end
