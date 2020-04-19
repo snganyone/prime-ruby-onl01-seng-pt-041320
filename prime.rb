e85fbd41f3a5d1d47a0ea035e3bdef87764aaf6f
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-19 16:50:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,11 +2,13 @@
 def prime?(number)
   if number == 0
     false
+  else
   (-2..number).each do |num|
     elsif number % num == 0
       false
     end
     true
   end
+end
 end
 # A prime number is divisible by itself and 1.
