76d4534f0a5fc475073b7737458b458bde554c4b
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-19 17:32:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+# Add code here!
+def prime?(number)
+  if number < 2
+    false
+  else
+    if (2..number-1).any?{|i| number % i == 0}
+      false
+    else
+      true
+    end
+  end
+end
+# A prime number is divisible by itself and 1.
